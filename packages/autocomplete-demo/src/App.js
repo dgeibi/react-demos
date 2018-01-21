d38504efefd66806dc0addc6aaed9450196df0b9
dgeibi <png.inside@gmail.com>
2018-01-21 16:48:53 +0800

    (autocomplete): update demo

--- packages/autocomplete-demo/src/App.js
+++ packages/autocomplete-demo/src/App.js
@@ -2,11 +2,19 @@ import React from 'react'
 import Autocomplete from '@dgeibi/autocomplete'
 import '@dgeibi/autocomplete/dist/@dgeibi/autocomplete.css'
 
+const codeA = 'A'.charCodeAt(0)
+
 function App() {
   return (
     <div className="wrapper">
       <section>
-        <Autocomplete items={['a', 'b', 'c']} />
+        <Autocomplete
+          items={Array.from(Array(100), (v, k) =>
+            Array.from(Array(Math.ceil(k / 2)), () =>
+              String.fromCharCode(codeA + k % 26)
+            ).join('')
+          )}
+        />
       </section>
     </div>
   )
